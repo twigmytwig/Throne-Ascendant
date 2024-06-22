42b535b353dc068997406526ee166c44036d4889
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2024-06-22 19:54:09 -0400

    feat:time increment is togglable

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <!--YEs I am aware this is dumb. It was throwing a stupid error about 
   player potentially being null and i didnt feel like fixing it so this is my temp solution-->
   <div v-if="player">
-    <Header  :player="player" :time="time"/>
+    <Header style="float:left; padding-right: 10px;" :player="player" :time="time"/>
+    <button  @click="toggleTimer">
+      {{ isTimerOn ? 'Stop Time' : 'Start Time' }}
+    </button>
   </div>
   <div v-else>
     Throne Ascendant
@@ -12,26 +15,30 @@
   <hr/>
   <NewPlayer v-if="isNew" @response="handlePlayerCreated"/>
   <br/>
+  <div v-if="player">
+    
+  </div>
 </template>
 
 <script setup lang="ts">
 
-import Header from "./components/StaticPageComponents/header-bar.vue"
-import NewPlayer from "./components/new-player.vue"
-import { Player } from "./TS/player"
-import { ref } from 'vue'
-import { Time } from './TS/Time'
+import Header from "./components/StaticPageComponents/header-bar.vue";
+import NewPlayer from "./components/new-player.vue";
+import { Player } from "./TS/player";
+import { ref } from 'vue';
+import { Time } from './TS/Time';
 
 let isNew = ref(true);
-const time = ref<Time>({Days: 0})
+const time = ref<Time>({Days: 0});
+const isTimerOn = ref(false);
 const player = ref<Player | null>(null);
-const playerName = ref("")
+const playerName = ref("");
+let intervalId: number | undefined;
 
 const handlePlayerCreated = (newPlayer: Player) => {
   isNew.value = false;
   player.value = newPlayer;
   playerName.value = player.value.name;
-  startIncrementing();
 }
 
 function incrementDays(){
@@ -39,7 +46,26 @@ function incrementDays(){
 }
 
 function startIncrementing(){
-  setInterval(incrementDays, 2000);
+  if (!intervalId) {
+        intervalId = setInterval(incrementDays, 2000);
+        isTimerOn.value = true;
+  }
+}
+
+function stopIncrementing(){
+      if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = undefined;
+        isTimerOn.value = false;
+      }
+}
+
+function toggleTimer(){
+      if (isTimerOn.value) {
+        stopIncrementing();
+      } else {
+        startIncrementing();
+      }
 }
 
 </script>
